9ca545a9c5ec9a7510bdb3dec52b082988859ca3
Tomas Dulka <dulajz@gmail.com>
2017-08-25 15:14:20 +0200

    first verion

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var moment = require("moment");
+
+var app = express();
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get("/:time", function(req, res){
+  var formattedTime = formatTime(req.params.time);
+  res.status(200).end(formattedTime);
+});
+
+app.get("/", function(req, res){
+  res.status(200).render("pages/index");
+});
+
+app.listen(8080);
+
+function formatTime(time){
+  var naturalTime = moment(time, "MMMM DD, YYYY").format("MMMM DD, YYYY");
+  var unixTime = moment(time, "x").valueOf();
+
+  if (naturalTime === time){
+    return JSON.stringify ({
+      "unix": moment(naturalTime, "MMMM DD, YYYY").valueOf(),
+      "natural": naturalTime
+    });
+  }
+
+  if(unixTime === +time){
+    return JSON.stringify ({
+      "unix": unixTime,
+      "natural": moment(unixTime, "x").format("MMMM DD, YYYY")
+    });
+  }
+
+  return JSON.stringify ({
+    "unix" : null,
+    "natural": null
+  });
+
+}
